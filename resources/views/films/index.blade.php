218f18066a30afc756c367a58650773c7d52a71e
Deryn Boscariol <derynb@rogers.com>
2024-08-09 19:26:47 -0400

    created films index view

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-</div>
+@extends('layouts/admin')
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1 class="display-2">
+                All Films
+            </h1>
+        </div>
+        <div class="col">
+            <h3>
+                <a href="{{ route('films.create') }}">Add a New Film</a>
+            </h3>
+        </div>
+    </div>
+    <div class="row">
+        @foreach($films as $film)
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width:18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{$film -> title}}
+                    </h5>
+                    <a href="{{ route('films.show', $film -> id) }}" class="card-link">See more</a>
+                    <a href="{{ route('films.edit', $film -> id) }}" class="card-link">Edit</a>
+                    <a href="{{ route('films.trash', $film -> id) }}" class="card-link">Delete</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+@endsection
