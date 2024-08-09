54bfda19afc6e7e03f3e0e91bc3283378150378d
Deryn Boscariol <derynb@rogers.com>
2024-08-09 19:44:24 -0400

    created trashed views

---
+++ resources/views/actors/trashed.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/admin')
+@section('content')
+<div class="row">
+    <div class="col">
+        <h1 class="display-2">
+            Deleted Actors
+        </h1>
+    </div>
+</div>
+<div class="row">
+    @foreach ($actors as $actor)
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{ $actor -> fname}} {{ $actor -> lname }}
+                    </h5>
+                    <a href="{{ route('actors.restore', $actor -> id) }}" class="card-link">Restore</a>
+                    <a href="{{ route('actors.destroy', $actor -> id)}}" class="card-link">Pernamently Delete</a>
+                    <form action="{{ route('actors.destroy', $actor -> id)}}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE')}}
+                        <button type="submit">Delete permanently</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+@endsection
---
+++ resources/views/films/trashed.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/admin')
+@section('content')
+<div class="row">
+    <div class="col">
+        <h1 class="display-2">
+            Deleted Films
+        </h1>
+    </div>
+</div>
+<div class="row">
+    @foreach ($films as $film)
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{ $film -> title}}
+                    </h5>
+                    <a href="{{ route('films.restore', $film -> id) }}" class="card-link">Restore</a>
+                    <a href="{{ route('films.destroy', $film -> id)}}" class="card-link">Pernamently Delete</a>
+                    <form action="{{ route('films.destroy', $film -> id)}}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE')}}
+                        <button type="submit">Delete permanently</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+@endsection
