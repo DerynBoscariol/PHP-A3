e2a3a066a4efd83b0bd17981023650f75b85feb5
Deryn Boscariol <derynb@rogers.com>
2024-08-09 18:10:37 -0400

    added show view and linked all views to index/each other

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -6,6 +6,11 @@
                 All Actors
             </h1>
         </div>
+        <div class="col">
+            <h3>
+                <a href="{{ route('actors.create') }}">Add a New Actor</a>
+            </h3>
+        </div>
     </div>
     <div class="row">
         @foreach($actors as $actor)
@@ -15,7 +20,7 @@
                     <h5 class="card-title">
                         {{$actor -> fname}} {{$actor -> lname}}
                     </h5>
-                    <p>From: {{ $actor-> country }}</p>
+                    <a href="{{ route('actors.show', $actor -> id) }}" class="card-link">See more</a>
                     <a href="{{ route('actors.edit', $actor -> id) }}" class="card-link">Edit</a>
                     <a href="{{ route('actors.trash', $actor -> id) }}" class="card-link">Delete</a>
                 </div>
--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+@extends('layouts/admin')
+@section('content')
+    <div class="row"> 
+        <div class="col">
+            <h1 class="display-2"> 
+                Actor Profile
+            </h1> 
+            <a href="{{ route('actors.index') }}">
+                <-- Back to all actors
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div>
+            <h5 class="card-title">{{ $actor -> fname }} {{ $actor -> lname }}</h5>
+            <p>Country of Origin: {{ $actor -> country }}</p>
+            <a href="{{ route('actors.edit', $actor -> id ) }}" class="card-link">Edit</a>
+            <a href="{{ route('actors.trash', $actor -> id )}}" class="card-link">Delete</a>
+        </div>
+    </div>
+@endsection
